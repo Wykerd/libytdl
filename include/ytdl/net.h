4d57d9c5ae56c8e89e8df5f0acaef64a06132d05
Daniel Wykerd <danielwykerd@gmail.com>
2021-06-30 23:53:29 +0200

    core: get watch url from id

--- include/ytdl/net.h
+++ include/ytdl/net.h
@@ -1,12 +1,26 @@
 #ifndef YTDL_NET_H
 #define YTDL_NET_H
 
+#define YTDL_WATCH_URL_SIZE sizeof("https://www.youtube.com/watch?v=") + 11
+#define YTDL_ID_SIZE 12
+#define YTDL_ID_LEN 11
+
 /**
  * Not very accurate but fast way to find youtube video url
  * 
  * May match invalid urls!
+ * @param url_str The URL cstring to get the ID from
+ * @param id ID string to populate
  * @returns 0 if successful. -1 if invalid url and 1 if not matched.
  */
-int ytdl_net_get_id_from_url (const char *url_str, char id[12]);
+int ytdl_net_get_id_from_url (const char *url_str, char id[YTDL_ID_SIZE]);
+
+/**
+ * Get the watch url for the specified id
+ * 
+ * @param url URL to populate
+ * @param id ID to use while populating url
+ */
+void ytdl_net_get_watch_url (char url[YTDL_WATCH_URL_SIZE], char id[YTDL_ID_SIZE]);
 
 #endif
--- src/net.c
+++ src/net.c
@@ -33,7 +33,12 @@ const char yt_valid_id_map[256] = {
     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 };
 
-int ytdl_net_get_id_from_url (const char *url_str, char id[12]) 
+void ytdl_net_get_watch_url (char url[YTDL_WATCH_URL_SIZE], char id[YTDL_ID_SIZE])
+{
+    sprintf(url, "%s%s", "https://www.youtube.com/watch?v=", id);
+}
+
+int ytdl_net_get_id_from_url (const char *url_str, char id[YTDL_ID_SIZE]) 
 {
     UriUriA uri;
     const char * error_pos;
@@ -55,15 +60,15 @@ fail:
     return 1;
 valid_host:
 
-    if (uri.pathTail->text.afterLast - uri.pathTail->text.first == 11) 
+    if (uri.pathTail->text.afterLast - uri.pathTail->text.first == YTDL_ID_LEN) 
     {
-        for (int i = 0; i < 11; i++) {
+        for (int i = 0; i < YTDL_ID_LEN; i++) {
             if (!yt_valid_id_map[uri.pathTail->text.first[i]])
                 goto invalid_path_tail;
         }
 
-        memcpy(id, uri.pathTail->text.first, 11);
-        id[11] = 0;
+        memcpy(id, uri.pathTail->text.first, YTDL_ID_LEN);
+        id[YTDL_ID_LEN] = 0;
         goto suffix_found;
     }
 
@@ -77,14 +82,14 @@ invalid_path_tail:
     cur = &queryList;
     do {
         if (((*cur)->key[0] == 'v') && ((*cur)->key[1] == 0)) {
-            if (strlen((*cur)->value) == 11) {
-                for (int i = 0; i < 11; i++) {
+            if (strlen((*cur)->value) == YTDL_ID_LEN) {
+                for (int i = 0; i < YTDL_ID_LEN; i++) {
                     if (!yt_valid_id_map[(*cur)->value[i]])
                         goto no_option;
                     }
 
-                    memcpy(id, (*cur)->value, 11);
-                    id[11] = 0;
+                    memcpy(id, (*cur)->value, YTDL_ID_LEN);
+                    id[YTDL_ID_LEN] = 0;
                     goto v_param_found;
                 }
         }
