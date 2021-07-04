7115b3bf7287ca1945cb23c3eba192005602af61
Daniel Wykerd <danielwykerd@gmail.com>
2021-07-04 03:33:03 +0200

    feat: cli improvements

--- src/cli.c
+++ src/cli.c
@@ -8,7 +8,8 @@ static void on_complete (ytdl_dl_ctx_t* ctx, ytdl_dl_video_t* vid)
 {
     ytdl_info_extract_formats(&vid->info);
 
-    printf("Best Audio: %s\n\nBest Video: %s\n\n", 
+    printf("#####################\nVideo ID: %s\n\nBest Audio: %s\n\nBest Video: %s\n\n", 
+        vid->id,
         ytdl_info_get_format_url(&vid->info, ytdl_info_get_best_audio_format(&vid->info)),
         ytdl_info_get_format_url(&vid->info, ytdl_info_get_best_video_format(&vid->info))
     );
@@ -25,21 +26,40 @@ static void on_complete_f (ytdl_dl_ctx_t* ctx, ytdl_dl_video_t* vid)
     ytdl_dl_shutdown(ctx);
 }
 
-int main ()
+int main (int argc, char **argv)
 {
+    char id[YTDL_ID_SIZE];
     ytdl_dl_ctx_t ctx;
+
+    if (argc < 2)
+    {
+        printf("Usage: %s [LINK]...\n", argv[0]);
+        return 0;
+    }
+
     ytdl_dl_ctx_init(uv_default_loop(), &ctx);
+
+    // Load JS Player Cache
     FILE *fd = fopen("./ytdl_cache.bin", "rb");
-    if (fd) {
+    if (fd) 
+    {
         ytdl_dl_player_cache_load_file(&ctx, fd);
         fclose(fd);
-    } else if (errno != ENOENT)
+    } 
+    else if (errno != ENOENT)
     {
         perror("JS Player Cache Error.");
         return 1;
     }
-    ytdl_dl_get_info (&ctx, "XDNFAujgJb0", on_complete_f);
-    //ytdl_dl_get_info (&ctx, "Y59vIAr8rAI", on_complete);
+
+    for (size_t i = 1; i < argc - 1; i++)
+    {
+        ytdl_net_get_id_from_url(argv[i], strlen(argv[i]), id);
+        ytdl_dl_get_info (&ctx, id, on_complete);
+    }
+    ytdl_net_get_id_from_url(argv[argc - 1], strlen(argv[argc - 1]), id);
+    ytdl_dl_get_info (&ctx, id, on_complete_f);
+    
     ytdl_dl_ctx_connect(&ctx);
     uv_run(uv_default_loop(), UV_RUN_DEFAULT);
 }
